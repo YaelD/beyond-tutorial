d16a5ed8f330ccdc878bc929397cc97246008e5d
Yael Davidov <yael.davidov97@gmail.com>
2022-02-10 22:05:16 +0200

    Adding models and migrations
    Adding models and migrations into our web application.

--- msgboard/migrations/0002_test_data.py
+++ msgboard/migrations/0002_test_data.py
@@ -3,22 +3,22 @@ from django.db import migrations, transaction
 
 class Migration(migrations.Migration):
 
-	dependencies = [
-		('msgboard', '0001_initial'),
-	]
+    dependencies = [
+        ('msgboard', '0001_initial'),
+    ]
 
-	def generate_data(apps, schema_editor):
-		from msgboard.models import Message
-	
-		test_data = [
-			('Test User1', 'A simple test message'),
-			('Test User2', 'Another simple test message'),
-		]
+    def generate_data(apps, schema_editor):
+        from msgboard.models import Message
 
-		with transaction.atomic():
-			for author, text in test_data:
-				Message(author=author, text=text).save()
-	
-	operations = [
-		migrations.RunPython(generate_data),
-	]
+        test_data = [
+            ('Test User1', 'A simple test message'),
+            ('Test User2', 'Another simple test message'),
+        ]
+
+        with transaction.atomic():
+            for author, text in test_data:
+                Message(author=author, text=text).save()
+
+    operations = [
+        migrations.RunPython(generate_data),
+    ]
